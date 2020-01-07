9aa264d269c3d406c06b9f58647014233d2d2e6a
Carlo Guzman <poptropicaman52@berkeley.edu>
2020-01-06 17:30:16 -0800

    adding available options to api call

--- api.py
+++ api.py
@@ -1,7 +1,15 @@
 # pip install alpha_vantage
+# https://github.com/RomelTorres/alpha_vantage
 from alpha_vantage.timeseries import TimeSeries
 
 key = '20NKEQN455YWFM4T'
-ts = TimeSeries(key)
-aapl = ts.get_intraday(symbol='AAPL')
+ts = TimeSeries(key, output_format='json', indexing_type='date')
+aapl, meta = ts.get_intraday(symbol='AAPL', outputsize='compact', interval='1min')
+
+#Print stock price info
 print(aapl)
+
+print('\n')
+
+#Print stock call info
+print(meta)
